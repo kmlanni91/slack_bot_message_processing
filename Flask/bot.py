194c71ac9a81c5b078d448bf3bfd3f6eb5d06680
Christopher Tran <christopher.long.tran@gmail.com>
2019-04-24 10:06:58 -0400

    created auth function

--- Flask/bot.py
+++ Flask/bot.py
@@ -5,6 +5,8 @@ import message
 
 from slackclient import SlackClient
 
+authed_teams = {}
+
 class Bot(object):
 
     def __init__(self):
@@ -19,3 +21,20 @@ class Bot(object):
         self.client = SlackClient("")
 
         self.messages = {}
+
+    def auth(self, code):
+
+        auth_response = self.client.api_call(
+            "oauth.access",
+            client_id=self.oauth["client_id"],
+            client_secret=self.oauth["client_secret"],
+            code=code
+        )
+
+        team_id = auth_response["team_id"]
+        authed_teams[team_id] = {"bot_token":
+            auth_response["bot"]["bot_access_token"]}
+
+        self.client = SlackClient(authed_teams[team_id]["bot_token"])
+
+    
